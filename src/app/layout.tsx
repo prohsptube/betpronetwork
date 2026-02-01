1b5d6be7d058515632d7c10be93f0e1f76b84c20
harbalhakeem <bhira567@gmail.com>
2026-02-02 04:47:59 +0500

    Fix component imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import WhatsAppButton from '@/components/WhatsAppButton'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import WhatsAppButton from '../components/WhatsAppButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
