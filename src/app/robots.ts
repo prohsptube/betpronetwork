a41aadffb92e5258a48d9c3b66561f68e17b8cdd
harbalhakeem <bhira567@gmail.com>
2026-02-02 23:22:57 +0500

    Prepare for Google Search Console: update robots.txt sitemap URL, protect /studio/ from indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,9 +6,9 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/'],
+        disallow: ['/studio/', '/studio/*', '/api/'],
       },
     ],
-    sitemap: 'https://betpronetwork.com/sitemap.xml',
+    sitemap: 'https://www.betpronetwork.com/sitemap.xml',
   }
 }
